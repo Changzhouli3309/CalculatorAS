bc171de394c23767ca97ab72c52005c889322c56
deltagare <deltagare@192.168.10.143>
2018-12-12 10:01:42 +0100

    test3

--- src/main/java/se/lexicon/CalculatorAS/App.java
+++ src/main/java/se/lexicon/CalculatorAS/App.java
@@ -4,8 +4,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        for(int i=0;i<4;i++) {
-        	System.out.println(++i);
+    	int sum = 0;
+    	
+        for(int i=1;i<6;i++) {
+        	if (i==1) {
+        		System.out.print(i);
+        		sum+=i;
+        	}else if (i==5) {
+        		sum+=i;
+        		System.out.print("+"+i+"="+sum);
+        	}else {
+        		sum+=i;
+        		System.out.print("+"+i);
+        	}
+        	
         }
     }
 }
